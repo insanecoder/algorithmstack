f7a963dd20f2e040609b20e26a5135c0311708de
Mukul Dilwaria <mukul.dilwaria@nagarro.com>
2018-02-04 09:51:33 +0530

    adding the coding page css

--- resources/views/blog/blogDescription.blade.php
+++ resources/views/blog/blogDescription.blade.php
@@ -20,18 +20,260 @@
 	@include('navHeader')
 	<div class="row">
 		@include('leftBar')
-		<div class="col-md-8">
-			<div class="panel panel-default">
-				<div class="panel-body">
-					<h1>{{$blog->title}}</h1>
-					{!! $blog->blogContent !!}
-				</div>
-			</div>
-		</div>
+
+
+<div class="col-md-7">
+            <div class="panel panel-default">
+              <div id="content-page-heading">
+                <h2  style="text-transform:capitalize; font-weight:600">Lorem ipsum dolor sit amet, consectetur</h2><br>
+                </div>
+                <div class="content-page">
+                    <div class="content-page-inner">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur
+                    </p>
+                    </div>
+                </div>
+                <div class="lang-choice">
+                    <h4>c++</h4>
+                </div>
+                <div class="program-container">
+                    <div class="program-container-inner">
+                            <div class="program">
+                                <div>
+                                    <code class="comment">//c++ program to</code>
+                                </div>
+                                <div>
+                                    <code class="comment">//print swastika program</code>
+                                </div>
+                                <div>
+                                    <code class="preprocessor">#include&lt;bits/stdc++.h&gt;</code>
+                                </div>
+                                <div>
+                                    <code class="keyword">using&nbsp;namespace</code>
+                                    <code class="plain">std;</code>
+                                </div>
+                                <div>
+                                    <code></code>
+                                </div>
+                                <div>
+                                    <code class="comment">//function to print swastika</code>
+                                </div>
+                                <div>
+                                    <code class="keyword">void</code>
+                                    <code class="plain">swastika(<code class="data-type">int</code>row, <code class="data-type">int</code>col);</code>
+                                </div>
+                                <div>
+                                    <code class="plain">{</code>
+                                </div>
+                                <div>
+                                    &emsp;<code class="keyword">for</code>
+                                    <code class="plain">(<code class="data-type">int</code>i = 0; i &lt; row; i++) {</code>
+                                </div>
+                                <div>
+                                    &emsp;&emsp;<code class="keyword">for</code>
+                                    <code class="plain">(<code class="data-type">int</code>j = 0; j &lt; col; j++) {</code>
+                                </div>
+                                <div>
+                                    <code></code>
+                                </div>
+                                <div>
+                                    &emsp;&emsp;<code class="comment">//checking if i &lt; row/2</code>
+                                </div>
+                                <div>
+                                    &emsp;&emsp;<code class="plain"><code class="keyword">if</code>( i &lt; row / 2 ) {</code>
+
+                                </div>
+                                <div>
+                                    <code></code>
+                                </div>
+                                <div>
+                                    &emsp;&emsp;<code class="comment">//checking if j &lt; col/2</code>
+                                </div>
+                                <div>
+                                    &emsp;&emsp;<code class="plain"><code class="keyword">if</code>( j &lt; col / 2 ) {</code>
+
+                                </div>
+                                <div>
+                                    <code></code>
+                                </div>
+                                <div>
+                                    &emsp;&emsp;<code class="comment">//checking if j &lt; col/2</code>
+                                </div>
+                                <div>
+                                    &emsp;&emsp;<code class="plain"><code class="keyword">if</code>( j &lt; col / 2 ) {</code>
+
+                                </div>
+                                <div>
+                                    &emsp;<code class="keyword">for</code>
+                                    <code class="plain">(<code class="data-type">int</code>i = 0; i &lt; row; i++) {</code>
+                                </div>
+                                <div>
+                                    &emsp;&emsp;<code class="keyword">for</code>
+                                    <code class="plain">(<code class="data-type">int</code>j = 0; j &lt; col; j++) {</code>
+                                </div>
+                                <div>
+                                    <code></code>
+                                </div>
+                                <div>
+                                    &emsp;&emsp;<code class="comment">//checking if i &lt; row/2</code>
+                                </div>
+                                <div>
+                                    &emsp;&emsp;<code class="plain"><code class="keyword">if</code>( i &lt; row / 2 ) {</code>
+
+                                </div>
+                                <div>
+                                    <code class="comment">//c++ program to</code>
+                                </div>
+                                <div>
+                                    <code class="comment">//print swastika program</code>
+                                </div>
+                                <div>
+                                    <code class="preprocessor">#include&lt;bits/stdc++.h&gt;</code>
+                                </div>
+                                <div>
+                                    <code class="keyword">using&nbsp;namespace</code>
+                                    <code class="plain">std;</code>
+                                </div>
+                                <div>
+                                    <code></code>
+                                </div>
+                                <div>
+                                    <code class="comment">//function to print swastika</code>
+                                </div>
+                                <div>
+                                    <code class="keyword">void</code>
+                                    <code class="plain">swastika(<code class="data-type">int</code>row, <code class="data-type">int</code>col);</code>
+                                </div>
+                                <div>
+                                    <code class="plain">{</code>
+                                </div>
+                                <div>
+                                    &emsp;<code class="keyword">for</code>
+                                    <code class="plain">(<code class="data-type">int</code>i = 0; i &lt; row; i++) {</code>
+                                </div>
+                                <div>
+                                    &emsp;&emsp;<code class="keyword">for</code>
+                                    <code class="plain">(<code class="data-type">int</code>j = 0; j &lt; col; j++) {</code>
+                                </div>
+                                <div>
+                                    <code></code>
+                                </div>
+                                <div>
+                                    &emsp;&emsp;<code class="comment">//checking if i &lt; row/2</code>
+                                </div>
+                                <div>
+                                    &emsp;&emsp;<code class="plain"><code class="keyword">if</code>( i &lt; row / 2 ) {</code>
+
+                                </div>
+                                <div>
+                                    <code></code>
+                                </div>
+                            </div>
+
+                    </div>
+                    <div class="program-output">
+                        <div class="program-output-inner">
+                            <div class="output-heading">
+                                <h6>output:</h6>
+                            </div>
+                            <div class="output-content">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit ess
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              <!-- </div> -->
+              <!-- <div class="tag">
+                <span class="ml"></span>
+              <span class="badge mousePointer customBadge ml1 mb2">Greedy</span>
+              <span class="badge mousePointer customBadge ml1 mb2">Greedy</span>
+              <span class="badge mousePointer customBadge ml1 mb2">Greedy</span>
+              </div> -->
+
+            <div class="controls">
+                <div class="previous-topic">
+                    <button class="butn-ctrl butn-blue">&lt;&lt; previous post</button>
+                </div>
+                <div class="next-topic">
+                    <button class="butn-ctrl butn-blue"> next post &gt;&gt;</button>
+                </div>
+            </div>
+            </div>
+                <div class="social-sharing">
+                <h3>share this post</h3>
+                    <div class="social-buttons">
+                        <div class="social-box">
+                            <a href="#"><i class="fa fa-facebook fa-3x share" aria-hidden="true"></i></a>
+                        </div>
+                        <div class="social-box">
+                            <a href="#"><i class="fa fa-twitter fa-3x share" aria-hidden="true"></i></a>
+                        </div>
+                        <div class="social-box">
+                            <a href="#"><i class="fa fa-linkedin fa-3x share" aria-hidden="true"></i></a>
+                        </div>
+                        <div class="social-box">
+                            <a href="#"><i class="fa fa-instagram fa-3x share" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                </div>
+        </div>
+
+        @include('rightBar')
+
+
 	</div>
 </div>
+@include('footer')
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src ="{{ asset('js/jquery.easing.1.3.js') }}" />
+    <script src ="{{ asset('js/custom.js') }}" />
 </body>
 </html>
